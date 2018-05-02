0dbcf9ea453f02483b8e7ab940a4d9dfccd85418
Miguel Collado <m.collado.gomez@gmail.com>
2018-05-02 19:58:22 +0200

    robots.txt, humans.txt

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,22 @@
 from django.conf.urls import url
 from website import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'', views.home)
+    url(
+        r'robots.txt$',
+        TemplateView.as_view(
+            template_name="website/robots.txt",
+            content_type="text/plain"
+        ), name="robots_file"
+    ),
+    url(
+        r'humans.txt$',
+        TemplateView.as_view(
+            template_name="website/humans.txt",
+            content_type="text/plain"
+        ), name="humans_file"
+    ),
+    url(r'', views.home),
 ]
